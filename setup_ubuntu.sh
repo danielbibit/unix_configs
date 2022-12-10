6a991a21e1fbdba6ef76403483a192e81a68ecd3
Daniel Moraes <danielbibit@gmail.com>
2022-12-10 03:32:52 -0300

    Create .bashrc link

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -4,6 +4,6 @@
 sudo ln -sf $PWD/vimrc /etc/vim/vimrc
 sudo ln -sf $PWD/tmux.conf /etc/tmux.conf
 
-# cp .bashrc /home/$user/
 # cp .bash_profile /home/$user/
+ln -sf $PWD/.bashrc /home/$(whoami)/.bashrc
 ln -sf $PWD/.gitconfig /home/$(whoami)/.gitconfig
