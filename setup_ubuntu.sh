d860fe63f7213c0e1cafa25ab77d3ca7360329b8
Daniel Moraes <danielbibit@gmail.com>
2022-12-09 22:25:07 -0300

    User whoami to setup user specific files

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -1,9 +1,9 @@
-echo 'Name of the user to config:'
-read user
+# echo 'Name of the user to config:'
+# read user
 
 sudo ln -sf $PWD/vimrc /etc/vim/vimrc
 sudo ln -sf $PWD/tmux.conf /etc/tmux.conf
 
 # cp .bashrc /home/$user/
 # cp .bash_profile /home/$user/
-ln -sf $PWD/.gitconfig /home/$user/.gitconfig
+ln -sf $PWD/.gitconfig /home/$(whoami)/.gitconfig
