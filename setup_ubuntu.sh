8867a9fc3f57488139961393aea1185a721b5a39
Daniel Moraes <danielbibit@gmail.com>
2022-12-10 09:06:38 -0300

    Add configs for ranger

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -3,7 +3,6 @@
 
 ln -sf $PWD/vimrc /home/$(whoami)/.vimrc
 ln -sf $PWD/tmux.conf /home/$(whoami)/.tmux.conf
-# cp .bash_profile /home/$user/
-
+ln -sf $PWD/ranger/rc.conf /home/$(whoami)/.config/ranger/rc.conf
 ln -sf $PWD/.bashrc /home/$(whoami)/.bashrc
 ln -sf $PWD/.gitconfig /home/$(whoami)/.gitconfig
