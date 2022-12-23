851c5c4d6d89d72338a67743f47073991f2a3a67
danielbibit <danielbibit@gmail.com>
2022-12-23 04:28:40 -0300

    Add keyboard shortcuts for windows

--- setup_windows.ps1
+++ setup_windows.ps1
@@ -1,1 +1,2 @@
-New-Item -Path 'C:\Users\danie\.wezterm.lua' -ItemType SymbolicLink -Value C:\Daniel\Code\unix_configs\.wezterm.lua
+New-Item -Path 'C:\Users\danie\.wezterm.lua' -ItemType SymbolicLink -Value C:\Daniel\Code\unix_configs\.wezterm.lua -Force
+# New-Item -Path 'C:\Users\danie\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup' -ItemType SymbolicLink -Value C:\Daniel\Code\unix_configs\shortcuts\win.ahk -Force
---
+++ shortcuts/win.ahk
@@ -0,0 +1,11 @@
+; https://wiki.archlinux.org/title/Xbindkeys
+
+; Ctrl + alt + c => Launch calculator
+^!c::
+Run C:\Program Files\Qalculate\qalc.exe
+Return
+
+; Ctrl + alt + t => Launch terminal
+^!t::
+Run C:\Program Files\WezTerm\wezterm-gui.exe
+Return
