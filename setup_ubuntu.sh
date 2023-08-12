6ae05f265a66b7f56e8b1a3e0d453c7f001aff46
Daniel Moraes <danielbibit@gmail.com>
2023-08-12 02:36:13 -0300

    Add software install to setup ubuntu

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -1,6 +1,7 @@
 # echo 'Name of the user to config:'
 # read user
-
+sudo apt update
+sudo apt install vim curl ranger tmux -y
 ln -sf $PWD/.wezterm.lua /home/$(whoami)/.wezterm.lua
 ln -sf $PWD/vimrc /home/$(whoami)/.vimrc
 ln -sf $PWD/tmux.conf /home/$(whoami)/.tmux.conf
