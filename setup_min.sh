566b58ba51a291710f88e2887badb670b0c0c8cd
Daniel Moraes dos Santos <danielbibit@gmail.com>
2025-04-27 04:43:08 +0000

    Remove vim plugin install from setup_min.sh

--- setup_min.sh
+++ setup_min.sh
@@ -14,18 +14,28 @@ elif command -v apt-get >/dev/null 2>&1; then
     apt-get update
     apt-get install -y $PACKAGES
 
+    LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+    curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+    tar xf lazygit.tar.gz lazygit
+    install lazygit -D -t /usr/local/bin/
+    rm lazygit.tar.gz
+    rm lazygit
 else
     echo "Unknown package manager. Cannot install packages."
     exit 1
 fi
 
 mkdir -p $HOME/.vim/config
-ln -sf $PWD/vim/*.vim $HOME/.vim/config/
+# ln -sf $PWD/vim/*.vim $HOME/.vim/config/
 ln -sf $PWD/vim/vimrc $HOME/.vimrc
+sed -i '/source ~\/\.vim\/config\/plugins.vim/d' "$HOME/.vimrc"
+sed -i '/source ~\/\.vim\/config\/keymaps.vim/d' "$HOME/.vimrc"
+
+mkdir -p $HOME/.config/ranger
+ln -sf $PWD/ranger/rc.conf $HOME/.config/ranger/rc.conf
 
 ln -sf $PWD/.wezterm.lua $HOME/.wezterm.lua
 ln -sf $PWD/tmux.conf $HOME/.tmux.conf
-ln -sf $PWD/ranger/rc.conf $HOME/.config/ranger/rc.conf
 ln -sf $PWD/.bashrc $HOME/.bashrc
 ln -sf $PWD/nvim $HOME/.config/
 
