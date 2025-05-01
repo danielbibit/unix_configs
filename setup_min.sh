cfb9e3b204921982c8f2b5fd79100b9dc45c014b
Daniel Moraes <danielbibit@gmail.com>
2025-05-01 18:05:35 +0000

    Add television install and remove lazygit alpine install

--- setup_min.sh
+++ setup_min.sh
@@ -7,23 +7,31 @@ if command -v apk >/dev/null 2>&1; then
     echo "Detected apk (Alpine Linux)"
     apk update
     apk add --no-cache $PACKAGES
-    apk add lazygit
 
 elif command -v apt-get >/dev/null 2>&1; then
     echo "Detected apt-get (Debian/Ubuntu)"
     apt-get update
     apt-get install -y $PACKAGES
-
-    LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
-    curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-    tar xf lazygit.tar.gz lazygit
-    install lazygit -D -t /usr/local/bin/
-    rm lazygit.tar.gz
-    rm lazygit
 else
     echo "Unknown package manager. Cannot install packages."
     exit 1
 fi
+
+# install lazygit
+LAZYGIT_VER=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VER}/lazygit_${LAZYGIT_VER}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+install lazygit -D -t /usr/local/bin/
+rm lazygit.tar.gz
+rm lazygit
+
+# Install television
+TELEVISION_VER=`curl -s "https://api.github.com/repos/alexpasmantier/television/releases/latest" | grep '"tag_name":' | sed -E 's/.*"tag_name": "([^"]+)".*/\1/'`
+curl -LO https://github.com/alexpasmantier/television/releases/download/$TELEVISION_VER/tv-$TELEVISION_VER-x86_64-unknown-linux-musl.tar.gz
+tar -xzf tv-$TELEVISION_VER-x86_64-unknown-linux-musl.tar.gz
+mv tv-$TELEVISION_VER-x86_64-unknown-linux-musl/tv /usr/local/bin/
+rm tv-$TELEVISION_VER-x86_64-unknown-linux-musl.tar.gz
+rm -rf tv-$TELEVISION_VER-x86_64-unknown-linux-musl
 
 mkdir -p $HOME/.vim/config
 ln -sf $PWD/vim/*.vim $HOME/.vim/config/
