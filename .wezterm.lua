e3e33d90b6296e3183652bc47fd4ce5eb785f95e
Daniel Moraes <danielbibit@gmail.com>
2026-01-12 01:53:53 -0300

    Add transparency and hide pane to wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -8,6 +8,10 @@ if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
 end
 
 return {
+    -- fix webGL crash on windows 11 ?
+    --front_end = "WebGpu",
+    --webgpu_power_preference = "HighPerformance",
+
     leader = {
         key = 'a',
         mods = 'CTRL',
@@ -25,19 +29,34 @@ return {
     cursor_blink_ease_in = "Constant",
     cursor_blink_ease_out = "Constant",
 
+    window_frame = {
+        active_titlebar_bg = '#333333',
+
+        inactive_titlebar_bg = '#333333',
+    },
+
+    inactive_pane_hsb = {
+      saturation = 0.75,
+      brightness = 0.55,
+    },
+
     window_padding = {
         left = 0,
         right = 0,
         top = 0,
         bottom = 0,
     },
-    -- color_scheme = "Mashup Colors (terminal.sexy)",
-    -- color_scheme = "Mariana",
-    -- color_scheme = "Macintosh (base16)",
-    -- color_scheme = "Materia (base16)",
-    color_scheme = "MaterialDark",
-    -- color_scheme = "MaterialDarker",
-    -- color_scheme = "MaterialOcean",
+
+    window_background_opacity = 0.93,
+    text_background_opacity = 0.85,
+
+     color_scheme = "Mashup Colors (terminal.sexy)",
+     --color_scheme = "Mariana",
+     --color_scheme = "Macintosh (base16)",
+     --color_scheme = "MaterialDark",
+     --color_scheme = "MaterialDarker",
+     --color_scheme = "MaterialOcean",
+
     keys = {
         {
           key = '|',
@@ -63,8 +82,11 @@ return {
 
 
         { key = 'n', mods = 'LEADER', action = act.SpawnTab 'CurrentPaneDomain' },
+        { key = 'x', mods = 'LEADER', action = act.CloseCurrentTab { confirm = false }},
         { key = 'Tab', mods = 'SHIFT', action = act.ActivateTabRelative(1) },
 
-        { key = 'c', mods = 'LEADER', action = act.ActivateCopyMode },
+        { key = 'y', mods = 'LEADER', action = act.ActivateCopyMode },
+
+        { key = 'z', mods = 'LEADER', action = act.TogglePaneZoomState },
     },
 }
