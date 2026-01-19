9f724a51c715561e45e74e41db96322e3f36904b
Daniel Moraes <danielbibit@gmail.com>
2026-01-18 23:42:24 -0300

    Add lazygit config

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -7,6 +7,7 @@ ln -sf $PWD/tmux.conf $HOME/.tmux.conf
 ln -sf $PWD/ranger/rc.conf $HOME/.config/ranger/rc.conf
 ln -sf $PWD/.bashrc $HOME/.bashrc
 ln -sf $PWD/nvim $HOME/.config/
+ln -sf $PWD/lazygit.yml /$HOME/.config/lazygit/config.yml
 
 mkdir -p $HOME/.vim/config
 ln -sf $PWD/vim/*.vim $HOME/.vim/config/
