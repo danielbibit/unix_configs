101ffe69eea1373fe8121f8a4160c667e4d4c816
Daniel Moraes dos Santos <danielbibit@gmail.com>
2026-01-23 17:10:33 -0300

    Improve tmux configs

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -81,3 +81,13 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
+-- Remove trailing whitespace on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function()
+    local pos = vim.api.nvim_win_get_cursor(0)
+    vim.cmd([[%s/\s\+$//e]])
+    vim.api.nvim_win_set_cursor(0, pos)
+  end,
+})
+
